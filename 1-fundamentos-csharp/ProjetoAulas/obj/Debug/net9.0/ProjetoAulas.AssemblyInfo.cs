d4405c5ced977cd83673afca2aa80f5c96e1f0c4
Jeferson Almeida <jefferson_qi3@hotmail.com>
2025-02-13 14:17:19 -0300

    Trabalhandop com variáveis

--- 1-fundamentos-csharp/ProjetoAulas/obj/Debug/net9.0/ProjetoAulas.AssemblyInfo.cs
+++ 1-fundamentos-csharp/ProjetoAulas/obj/Debug/net9.0/ProjetoAulas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoAulas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04533fd45d06630cca81588069f48dfb81cac19b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9593083b3e3fd2a793fdb8f5e715d3c3ba5ffd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoAulas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoAulas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
