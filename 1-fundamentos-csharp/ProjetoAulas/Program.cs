c88f01b4228408d97622fe18e9e010013caa360c
Jeferson Almeida <jefferson_qi3@hotmail.com>
2025-02-14 15:55:17 -0300

    Operadores Aritiméticos

--- 1-fundamentos-csharp/ProjetoAulas/Program.cs
+++ 1-fundamentos-csharp/ProjetoAulas/Program.cs
@@ -42,3 +42,66 @@
 
 /* Escreve a mesangem Hello, World no console 
     Console.WriteLine("Hello, World!"); */
+
+////////////////////////////////////////////////////
+// OPERADORES DE ATRIBUIÇÃO
+// Declaramos a variável idade que recebe o valor 43
+//int idade = 43;
+// Declaramos a variável maiorIdade como boolean 
+//bool maiorIdade;
+// Exibimos ambos no console, atribuindo valor ao booleano
+//Console.WriteLine(idade);
+//Console.WriteLine(maiorIdade = true);
+
+////////////////////////////////////////////////////
+// OPERADORES ARITIMÉTICOS
+
+// int a = 5, b = 10, c = 15, d = 20; // declaramos quatro variáveis do tipo int
+// Console.WriteLine(a + d); // operação de soma
+// Console.WriteLine(c - a); // operação de subtração
+// Console.WriteLine(b * c); // operação de multiplicação
+// Console.WriteLine(d / b); // operação de divisão
+// Console.WriteLine(c % b); // operação de módulo (resto de divisão)
+ 
+// // ODERDEM DE OPERAÇÕES MATEMÁTICAS
+// int a = 5, b = 10, c = 15, d = 20;
+
+// Console.WriteLine(a + b * c); // A operação de multiplicação será executada primeiro
+// Console.WriteLine((a + b) * c); // A operação de soma será executada primeiro porque está entre parênteses
+
+// Operadores aritiméticos de Atribuição reduzida
+// int x = 5;
+// x += 5; // é a mesma coisa que x = x + 5
+// Console.WriteLine("Valor do x = " + x);
+
+// Operadores incrementais e decrementais
+
+// Console.WriteLine("Pré-Incremento:\n");
+
+// int x = 0;
+// Console.WriteLine("x = " + x);
+
+// Console.WriteLine("++x +20 = \n");
+// Console.WriteLine(++x +20 + "\n");
+
+// Console.WriteLine("Pós-Incremento:\n");
+// x = 0;
+
+// Console.WriteLine("\nx++ +20 = \n");
+// Console.WriteLine(x++ + 20 + "\n");
+
+// Console.WriteLine("Pré-Decremento:\n");
+// x = 0;
+
+// Console.WriteLine("x = " + x);
+
+// Console.WriteLine("--x +20 = \n");
+// Console.WriteLine(--x + 20 + "\n");
+
+// Console.WriteLine("Pós-Decremento:\n");
+// x = 0;
+
+// Console.WriteLine("x-- +20 = \n");
+// Console.WriteLine(x-- + 20 + "\n");
+
+
